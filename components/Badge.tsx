fed4e61b9953af17ca43f7f5898d91759b829fec
KWIZERA Olivier <kwizeraolivier2119@gmail.com>
2025-04-04 00:14:03 +0200

    modifying the badge.

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import { FaCheckCircle } from "react-icons/fa";
 
 const Badge = () => {
   return (
-    <div
-      className="absolute top-30 right-10 flex flex-col items-center justify-center p-4 bg-white/30 backdrop-blur-lg rounded-lg shadow-md"
-    >
-      <h1 className="text-[#FF8B00] text-3xl font-extrabold">200k+</h1>
-      <p className="text-white text-lg font-medium">People Delivered</p>
+    <div className="absolute top-[7.5rem] right-10 flex flex-col items-center justify-center p-4 bg-white/10 backdrop-blur-lg rounded-lg shadow-md">
+      <h1 className="text-2xl font-bold text-[#FF8B00] flex items-center">
+        <FaCheckCircle className="mr-2" /> 200k+
+      </h1>
+      <p className="text-sm text-gray-600">People delivered</p>
     </div>
   );
 };
