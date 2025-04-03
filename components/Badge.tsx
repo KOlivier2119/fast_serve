c55052abccb740c46e61228341837bce09e3f9ea
KWIZERA Olivier <kwizeraolivier2119@gmail.com>
2025-04-03 20:37:29 +0200

    link and badge components.

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Badge = () => {
   return (
-    <div className='flex flex-col '>
-      
+    <div className='flex flex-col p-2 bg-white bg-opacity-75'>
+      <h1 className='text-[#FF8B00]'>200k+</h1>
+      <p>People Delivered</p>
     </div>
   )
 }
---
+++ components/LinkGroup.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LinkGroup = () => {
+  return (
+    <div className='flex'>
+      <input type="text" />
+      <button>Search</button>
+    </div>
+  )
+}
+
+export default LinkGroup
